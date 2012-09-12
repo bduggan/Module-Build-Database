ae17ae4343b4a8e7280f8f164d3c842f57efa53a
Brian Duggan <brian.duggan@nasa.gov>
2012-09-12 12:11:02 -0400

    version 0.35

--- lib/Module/Build/Database.pm
+++ lib/Module/Build/Database.pm
@@ -178,7 +178,7 @@ use strict;
 use Module::Build::Database::Helpers qw/debug info/;
 use base 'Module::Build';
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 sub new {
     my $class = shift;
