07e1b4c7f0e97525a1b8316e395afb847933730f
Brian Duggan <bduggan@usgcrp.gov>
2013-06-25 12:04:51 -0400

    version 0.46

--- lib/Module/Build/Database.pm
+++ lib/Module/Build/Database.pm
@@ -199,7 +199,7 @@ use strict;
 use Module::Build::Database::Helpers qw/debug info/;
 use base 'Module::Build';
 
-our $VERSION = '0.45';
+our $VERSION = '0.46';
 
 __PACKAGE__->add_property(database_object_class => default => "");
 
